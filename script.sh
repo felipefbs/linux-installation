e8139b90212b9e874c6d8f23ea1aa5e543d4aaf7
felipefbs <felipeffbs3x@gmail.com>
2020-07-12 15:13:33 -0300

    added google chrome

--- script.sh
+++ script.sh
@@ -5,13 +5,17 @@ cd $HOME/Downloads/linux-installation
 ## Numix incon theme circle PPA
 sudo add-apt-repository -y ppa:numix/ppa
 
-## Visual Studio Code repositorie
+## Visual Studio Code repository
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 aptitude install apt-transport-https
 
-## Brave Browser repositorie
+## Google Chrome repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+## Brave Browser repository
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
@@ -38,7 +42,8 @@ sudo aptitude install -y git \
                          psensor \
                          putty \
                          code \
-                         brave-browser
+                         brave-browser \
+                         google-chrome-stable
 
 ## flatpak packages
 flatpak install flathub com.discordapp.Discord \
